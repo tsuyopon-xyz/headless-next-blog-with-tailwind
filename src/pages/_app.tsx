f9c3cc5efb24df966828a3ad9ac63bd26ac3b871
Tsuyoshi Maeda <oasis5tsuyoshi@gmail.com>
2021-10-05 15:29:47 +0900

    Impl Footer.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,14 @@ import { VFC } from 'react';
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { Header } from '@/components/common/Header';
+import { Footer } from '@/components/common/Footer';
 
 const MyApp: VFC<AppProps> = ({ Component, pageProps }) => {
   return (
     <>
       <Header />
       <Component {...pageProps} />
+      <Footer />
     </>
   );
 };
